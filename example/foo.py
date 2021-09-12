52d62a16d071d1b85047f4a58eadb25dcea33c6d
b-mclellan <brendan.mclellan@gmail.com>
2021-09-12 19:03:53 -0400

    Update foo.py

--- example/foo.py
+++ example/foo.py
@@ -1,6 +1,11 @@
-import check50
+import check50 # import the check50 module
 
-@check50.check()
-def hello_world():
-    """hello world"""
-    check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
+@check50.check() # tag the function below as check50 check
+def exists(): # the name of the check
+    """description""" # this is what you will see when running check50
+    check50.exists("hello.py") # the actual check
+
+@check50.check(exists) # only run this check if the exists check has passed
+def prints_hello():
+    """Does the program print "Hello, world\\n"? """
+    check50.run("python3 hello.py").stdout("[Hh]ello, [Ww]orld!?\n", regex=True).exit(0)
