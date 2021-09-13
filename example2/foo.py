304e3c99a61d460e334ad4ff42a688b618a7fb92
b-mclellan <brendan.mclellan@gmail.com>
2021-09-13 11:18:05 -0400

    Update foo.py

--- example2/foo.py
+++ example2/foo.py
@@ -10,7 +10,7 @@ def prints_hello2():
     """Does the program print "Hello, world2\\n"? """
     from re import match
  
-    expected = "[Hh]ello, world2\n"
+    expected = "[Hh]ello, world2\\n"
     actual = check50.run("python3 hello2.py").stdout()
     if not match(expected, actual):
         help = None
