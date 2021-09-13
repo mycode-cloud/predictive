5917e5931b1a02359e00ba66f8dae47829ac1f0d
b-mclellan <brendan.mclellan@gmail.com>
2021-09-13 10:51:41 -0400

    Create foo.py

---
+++ example2/foo.py
@@ -0,0 +1,11 @@
+import check50 # import the check50 module
+
+@check50.check() # tag the function below as check50 check
+def exists(): # the name of the check
+    """file exists""" # this is what you will see when running check50
+    check50.exists("hello2.py") # the actual check
+
+@check50.check(exists) # only run this check if the exists check has passed
+def prints_hello():
+    """program prints "Hello, world2\\n"? """
+    check50.run("python3 hello2.py").stdout("[Hh]ello, [Ww]orld2\n", regex=True).exit(0)
