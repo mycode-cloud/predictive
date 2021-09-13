ebaac8464bdd71cbe923e693958f05f051dd657e
b-mclellan <brendan.mclellan@gmail.com>
2021-09-13 13:47:14 -0400

    Update foo.py

--- example2/foo.py
+++ example2/foo.py
@@ -7,15 +7,15 @@ def exists(): # the name of the check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def prints_hello2():
-    """Does the program print "Hello, world2\\n"? """
+    """Does the program print "Hello, world2"? """
     import re
  
-    expected = "Hello, world2\n"
+    expected = "Hello, world2"
     actual = check50.run("python3 hello2.py").stdout()
-    if not re.match(expected, actual,re.M):
+    if not re.match(expected, actual):
         help = None
         if re.match(expected[:-1], actual):
             help = r"did you forget a newline ('\n') at the end of your printf string?"
         raise check50.Mismatch("Hello, world2\n", actual, help=help)
-    if re.match(expected,actual,re.M):
+    if re.match(expected,actual):
         print(True)
