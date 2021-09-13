2537c23c8a5cf82c8addd5d1d361c76a59f3c283
b-mclellan <brendan.mclellan@gmail.com>
2021-09-13 11:03:33 -0400

    Update foo.py

--- example2/foo.py
+++ example2/foo.py
@@ -8,4 +8,4 @@ def exists(): # the name of the check
 @check50.check(exists) # only run this check if the exists check has passed
 def prints_hello2():
     """program prints "Hello, world2\\n"? """
-    check50.run("python3 hello2.py").stdout("[Hh]ello, [Ww]orld2\n", regex=True).exit(0)
+    check50.run("python3 hello2.py").stdout("[Hh]ello, [Ww]orld2\\n", regex=True).exit(0)
