d8f55d9df624856f39e5368e1dac35be23c849aa
b-mclellan <brendan.mclellan@gmail.com>
2021-10-26 15:34:48 -0400

    Update foo.py

--- 1_python_basics/Question2/foo.py
+++ 1_python_basics/Question2/foo.py
@@ -1,5 +1,12 @@
 import check50 # import the check50 module
 import check50.py
+import re
+
+
+def search_n(num):
+    # regex that matches `num` not surrounded by any other numbers
+    # (so search_n(2) won't match e.g. 123)
+    return fr"(?<!\d){num}(?!\d)"
 
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
@@ -16,9 +23,10 @@ def check_cases(exists):
     check50.run("python3 Question2.py").stdout("78").exit()
     
     actual = check50.run("python3 Question2.py").stdout()
-    if 66 not in actual:
+ 
+    if not re.search(search_n(66), actual):
         help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10,11]"
         raise check50.Mismatch("66", actual, help=help)
-    if 78 not in actual:
+    if not re.search(search_n(78), actual):
         help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10,11,12]"
         raise check50.Mismatch("78", actual, help=help)
