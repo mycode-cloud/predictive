656d8b5c27bef8d2d69c7d36c68476e81bcfde87
b-mclellan <brendan.mclellan@gmail.com>
2021-10-26 11:17:07 -0400

    Update foo.py

--- 1_python_basics/Question1/foo.py
+++ 1_python_basics/Question1/foo.py
@@ -3,9 +3,9 @@ import check50 # import the check50 module
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("hello.py") # the actual check
+    check50.exists("Question1.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def prints_hello():
     """Program prints "Hello, world!" """
-    check50.run("python3 hello.py").stdout("Hello, world!", regex=True).exit(0)
+    check50.run("python3 Question1.py").stdout("Hello, world!", regex=True).exit(0)
