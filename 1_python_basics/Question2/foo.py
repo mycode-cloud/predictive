b243b9e37d862f578a507053f2f3d8ea84055fd2
b-mclellan <brendan.mclellan@gmail.com>
2021-10-26 15:15:03 -0400

    Update foo.py

--- 1_python_basics/Question2/foo.py
+++ 1_python_basics/Question2/foo.py
@@ -14,3 +14,11 @@ def check_cases(exists):
     check50.run("python3 Question2.py").stdout("55").exit()
     check50.run("python3 Question2.py").stdout("66").exit()
     check50.run("python3 Question2.py").stdout("78").exit()
+    
+    actual = check50.run("python3 Question2.py").stdout()
+    if "66" not in actual:
+        help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10,11]"
+        raise check50.Mismatch("66", actual, help=help)
+    if "78" not in actual:
+        help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10,11,12]"
+        raise check50.Mismatch("78", actual, help=help)
