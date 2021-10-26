8478080963d8becbb3b5824ac360edc6c4fa3ee2
b-mclellan <brendan.mclellan@gmail.com>
2021-10-26 14:50:14 -0400

    Update foo.py

--- 1_python_basics/Question2/foo.py
+++ 1_python_basics/Question2/foo.py
@@ -7,7 +7,9 @@ def exists(): # the name of the check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases(exists):
-    """Program prints correct name length """
-    check50.run("python3 Question1.py").stdin("[1, 2, 3, 4, 5]").stdin("[6, 7, 8, 9, 10]").stdout("55").exit()
-    check50.run("python3 Question1.py").stdin("[1, 2, 3, 4, 5]").stdin("[6, 7, 8, 9, 10, 11]").stdout("66").exit()
-    check50.run("python3 Question1.py").stdin("[1, 2, 3, 4, 5]").stdin("[6, 7, 8, 9, 10, 11, 12]").stdout("78").exit()
+    """Program prints correct sum """
+    check50.include("Question2_Sol.txt")
+    check50.py.append_code("Question2.py", "Question2_Sol.py")
+    check50.run("python3 Question2.py").stdout("55").exit()
+    check50.run("python3 Question2.py").stdout("66").exit()
+    check50.run("python3 Question2.py").stdout("78").exit()
