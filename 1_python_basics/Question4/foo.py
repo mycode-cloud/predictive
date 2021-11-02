46f05ecfd70fb1635438ae1b5569cb8e2f2e729c
b-mclellan <brendan.mclellan@gmail.com>
2021-11-02 13:44:55 -0400

    Update foo.py

--- 1_python_basics/Question4/foo.py
+++ 1_python_basics/Question4/foo.py
@@ -15,12 +15,12 @@ def check_cases(exists):
     
     actual = check50.run("python3 Question4.py").stdout()
     
-    if "Johannes Jamal Galina" not in actual:
-        help = r"Your code does not work with the list ['Johannes', 'Jamal', 'Jamal', 'Johannes', 'Galina']."
-        raise check50.Missing("Johannes Jamal Galina", actual, help=help)
-    if "a b c" not in actual:
-        help = r"Your code does not work with the list ['a', 'a', 'a', 'b', 'b', 'c'].  Your code is being checked against several different lists."
+    if "25" not in actual:
+        help = r"Your code does not work for 'Johanne'."
+        raise check50.Missing("25", actual, help=help)
+    if "5" not in actual:
+        help = r"Your code does not work for 'John'.  Your code is being checked against several different cases."
         raise check50.Missing("a b c", actual, help=help)
     if "d e f g" not in actual:
-        help = r"Your code does not work with the ['d', 'd', 'd', 'd', 'e', 'e', 'e', 'f', 'f', 'g', 'g'].  Your code is being checked against several different lists."
+        help = r"Your code does not work for other dictionaries.  Your code is being checked against several different dictionaries."
         raise check50.Missing("d e f g", actual, help=help)
