0a24f08de80ef040b14561ce46b92521ca772753
b-mclellan <brendan.mclellan@gmail.com>
2021-11-09 13:13:17 -0500

    Update foo.py

--- 2_NumPy/Question1/foo.py
+++ 2_NumPy/Question1/foo.py
@@ -13,7 +13,7 @@ def check_cases(exists):
     check50.include("Question1_Sol.py")
     check50.py.append_code("Question1.py", "Question1_Sol.py")
     
-    actual = check50.run("python3 Question1.py").stdout(regex=True)
+    actual = check50.run("python3 Question1.py").stdout()
     expected1 = r'\[\[1,2\]\n \[3,4\]\]'
     expected2 = r'\[\[5,5\]\n \[5,5\]\]'
     expected3 = r'\[\[9,8\]\n \[7,6\]\]'
