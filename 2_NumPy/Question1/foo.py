2b050ce63df971bc5bda06bbe1e366f9e8732888
b-mclellan <brendan.mclellan@gmail.com>
2021-11-09 15:35:27 -0500

    Update foo.py

--- 2_NumPy/Question1/foo.py
+++ 2_NumPy/Question1/foo.py
@@ -8,10 +8,8 @@ def exists(): # the name of the check
     check50.exists("Question1.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
-def check_cases1(exists):
+def check_cases11(exists):
     """Program prints correct output for reshaped array 'a'"""
-    check50.include("Question1_Sol.py")
-    check50.py.append_code("Question1.py", "Question1_Sol.py")
     
     actual = check50.run("python3 Question1.py").stdout()
     expected1_1 = '[[1,2]'
@@ -20,20 +18,89 @@ def check_cases1(exists):
     if not re.search(expected1_1, actual) or not re.search(expected1_2, actual):
         help = r"Your code does not print the correct result for the reshaped array 'a'."
         raise check50.Missing('Reshaped array','Given output',help=help)
+      
+    
+    
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases12(exists):
+    """Program prints correct output for sum 'b+c'"""
+    
+    actual = check50.run("python3 Question1.py").stdout()
+    
+    expected2 = '[[5,5]'
+    
+    if not re.search(expected2, actual):
+        help = r"Your code does not print the correct result for the the sum 'b+c'."
+        raise check50.Missing('Sum array','Given output',help=help)
+        
+        
+        
         
 @check50.check(exists) # only run this check if the exists check has passed
-def check_cases2(exists):
+def check_cases21(exists):
+    """Program prints correct output for reshaped array 'a2'"""
+    
+    actual = check50.run("python3 Question1.py").stdout()
+    expected1_1 = '[[9,8]'
+    expected1_2 = '[7,6]]'
+    
+    if not re.search(expected1_1, actual) or not re.search(expected1_2, actual):
+        help = r"Your code does not print the correct result for the reshaped array 'a2'."
+        raise check50.Missing('Reshaped array','Given output',help=help)
+        
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases22(exists):
+    """Program prints correct output for sum 'b+c'"""
+    
+    actual = check50.run("python3 Question1.py").stdout()
+    
+    expected2 = '[[15,15]'
+    
+    if not re.search(expected2, actual):
+        help = r"Your code does not print the correct result for the the sum 'b2+c'."
+        raise check50.Missing('Sum array','Given output',help=help)
+        
+        
+        
+
+        
+        
+        
+        
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases31(exists):
+    """Program prints correct output for reshaped array 'a2'"""
+    check50.include("Question1_Sol.py")
+    check50.py.append_code("Question1.py", "Question1_Sol.py")
+    
+    actual = check50.run("python3 Question1.py").stdout()
+    expected1_1 = '[[2,3]'
+    expected1_2 = '[4,5]]'
+    
+    if not re.search(expected1_1, actual) or not re.search(expected1_2, actual):
+        help = r"Your code does not print the correct reshaped result for other possible arrays."
+        raise check50.Missing('Reshaped array','Given output',help=help)
+        
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases32(exists):
     """Program prints correct output for sum 'b+c'"""
     check50.include("Question1_Sol.py")
     check50.py.append_code("Question1.py", "Question1_Sol.py")
     
     actual = check50.run("python3 Question1.py").stdout()
     
-    expected2 = '[[5,5]'
-
+    expected2 = '[[15,15]'
     
     if not re.search(expected2, actual):
-        help = r"Your code does not print the correct result for the the sum 'b+c'."
+        help = r"Your code does not print the correct result for other possible arrays."
         raise check50.Missing('Sum array','Given output',help=help)
+
+
 
 
