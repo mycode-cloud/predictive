f331c295f8a46019209f456e370f73671f1938de
b-mclellan <brendan.mclellan@gmail.com>
2021-11-22 15:41:05 -0500

    Update foo.py

--- 1_python_basics/Question2/foo.py
+++ 1_python_basics/Question2/foo.py
@@ -15,7 +15,7 @@ def exists(): # the name of the check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases(exists):
-    """Program prints correct sum """
+    """Program prints correct sum - Test 1"""
     check50.include("Question2_Sol.py")
     check50.py.append_code("Question2.py", "Question2_Sol.py")
     
@@ -23,10 +23,34 @@ def check_cases(exists):
     
     if "55" not in actual:
         help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10]."
-        raise check50.Missing("55", actual, help=help)
+        raise check50.Missing("55", 'your output', help=help)       
+        
+        
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases2(exists):
+    """Program prints correct sum - Test 2"""
+    check50.include("Question2_Sol.py")
+    check50.py.append_code("Question2.py", "Question2_Sol.py")
+    
+    actual = check50.run("python3 Question2.py").stdout()
+    
     if "66" not in actual:
         help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10,11].  Your code is being checked against several different arrays."
-        raise check50.Missing("66", actual, help=help)
+        raise check50.Missing("66", 'your output', help=help)
+        
+        
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases3(exists):
+    """Program prints correct sum - Test 3"""
+    check50.include("Question2_Sol.py")
+    check50.py.append_code("Question2.py", "Question2_Sol.py")
+    
+    actual = check50.run("python3 Question2.py").stdout()
+    
     if "78" not in actual:
         help = r"Your code does not work with the arrays [1,2,3,4,5] and [6,7,8,9,10,11,12].  Your code is being checked against several different arrays."
-        raise check50.Missing("78", actual, help=help)
+        raise check50.Missing("78", 'your output', help=help)
