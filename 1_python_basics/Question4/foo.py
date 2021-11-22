888a63eef2b6e34ab134b5f7bdcef7311045273f
b-mclellan <brendan.mclellan@gmail.com>
2021-11-22 16:25:16 -0500

    Update foo.py

--- 1_python_basics/Question4/foo.py
+++ 1_python_basics/Question4/foo.py
@@ -13,26 +13,67 @@ def exists(): # the name of the check
     check50.exists("Question4.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
-def check_cases(exists):
+def check_cases():
     """Program prints correct number of points for organizer """
     check50.include("Question4_Sol.py")
     check50.py.append_code("Question4.py", "Question4_Sol.py")
     
     actual = check50.run("python3 Question4.py").stdout()
     expected1 = "25\n"
-    expected2 = "5\n"
-    expected3 = "55\n"
-    expected4 = "35\n"
     
     if not re.search(sep_num(25), actual):
         help = r"Your code does not print the correct result for 'Johannes'."
-        raise check50.Mismatch(expected1, actual, help=help)
+        raise check50.Mismatch("Correct output", "your output", help=help)
+        
+        
+        
+        
+        
+        
+@check50.check(check_cases) # only run this check if the exists check has passed
+def check_cases2():
+    """Program prints correct number of points for organizer """
+    check50.include("Question4_Sol.py")
+    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    
+    actual = check50.run("python3 Question4.py").stdout()
+    expected2 = "5\n"
+    
     if not re.search(sep_num(5), actual):
         help = r"Your code does not work for 'John'.  Your code is being checked against several different cases."
-        raise check50.Mismatch(expected2, actual, help=help)
+        raise check50.Mismatch("Correct output", "your output", help=help)
+        
+        
+        
+        
+        
+@check50.check(check_cases2) # only run this check if the exists check has passed
+def check_cases3():
+    """Program prints correct number of points for organizer """
+    check50.include("Question4_Sol.py")
+    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    
+    actual = check50.run("python3 Question4.py").stdout()
+    expected3 = "55\n"
+    
     if not re.search(sep_num(55), actual):
         help = r"Your code does not work for other dictionaries.  Your code is being checked against several different dictionaries."
-        raise check50.Mismatch(expected3, actual, help=help)
+        raise check50.Mismatch("Correct output", "your output", help=help)
+        
+        
+        
+        
+        
+        
+@check50.check(check_cases3) # only run this check if the exists check has passed
+def check_cases4():
+    """Program prints correct number of points for organizer """
+    check50.include("Question4_Sol.py")
+    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    
+    actual = check50.run("python3 Question4.py").stdout()
+    expected4 = "35\n"
+    
     if not re.search(sep_num(35), actual):
         help = r"Your code does not work for other dictionaries.  Your code is being checked against several different dictionaries."
-        raise check50.Mismatch(expected4, actual, help=help)
+        raise check50.Mismatch("Correct output", "your output", help=help)
