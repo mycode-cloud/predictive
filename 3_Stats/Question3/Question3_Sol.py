1ac924ddfab4af435f7915d61f4ba7500334433b
b-mclellan <brendan.mclellan@gmail.com>
2021-11-29 10:52:44 -0500

    Create Question3_Sol.py

---
+++ 3_Stats/Question3/Question3_Sol.py
@@ -0,0 +1,3 @@
+speeds3 = np.array([29.2,24.9,15.5,26.9,27.3,19,13.8,18.1,15.7,19,25.3,22.5,12,29.7,15.8])
+
+print('The variance and standard deviation of the data speeds is: ', get_var_dev(speeds3), ', respectively.')
