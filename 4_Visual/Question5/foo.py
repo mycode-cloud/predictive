b1dc15b39ef250fe0a0238eb33ebfedcedd8b52a
b-mclellan <brendan.mclellan@gmail.com>
2022-03-29 15:24:22 -0400

    Create foo.py

---
+++ 4_Visual/Question5/foo.py
@@ -0,0 +1,7 @@
+import check50 # import the check50 module
+import check50.py
+
+@check50.check() # tag the function below as check50 check
+def exists(): # the name of the check
+    """File exists""" # this is what you will see when running check50
+    check50.exists("Question5.py") # the actual check
