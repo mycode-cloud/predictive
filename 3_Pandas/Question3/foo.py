6d7d34e7b872889f23daa7e11df24255e5cf989f
mycode-cloud <brendan.mclellan@gmail.com>
2023-03-21 11:23:03 -0400

    Update foo.py

--- 3_Pandas/Question3/foo.py
+++ 3_Pandas/Question3/foo.py
@@ -10,17 +10,6 @@ def sep_num(num):
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question1.py") # the actual check
-
-@check50.check(exists) # only run this check if the exists check has passed
-def check_cases():
-    """Program prints the correct mean for 'speeds' - Test 1"""
-    
-    actual = check50.run("python3 Question1.py").stdout()
-    expected1 = 20.77333333333333
-    
-    if not re.search(sep_num(expected1), actual):
-        help = r"Your code does not print the correct mean for 'speeds'."
-        raise check50.Mismatch("Correct output", "your output", help=help)
+    check50.exists("Question3.py") # the actual check
 
         
