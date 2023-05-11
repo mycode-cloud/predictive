c972bd3a535927883dd065b3efce0155ce281ff3
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-11 19:56:38 -0400

    Update foo.py

--- 4_Pandas/Question1/foo.py
+++ 4_Pandas/Question1/foo.py
@@ -19,12 +19,7 @@ def check_cases11(exists):
     actual = check50.run("python3 Question1.py").stdout()
     expected = "The group of students with the highest mean scores in math, writing, and reading had a majority of female students."
     
-    checker = False
-    
-    if 'The group of students with the highest mean scores in math, writing, and reading had a majority of female students.' in actual:
-        checker = True
-    
-    if not checker:
+    if not expected in actual:
         help = r"Your code does not print the correct result for the reshaped array 'a'."
         raise check50.Missing("Reshaped array 'a'",'your output',help=help)
 
