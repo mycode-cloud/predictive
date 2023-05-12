e6411f39909919105513ede308c54cd32036c34e
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-12 09:30:48 -0400

    Update foo.py

--- 4_Pandas/Question1/foo.py
+++ 4_Pandas/Question1/foo.py
@@ -3,10 +3,8 @@ import check50.py
 import re
 import os
 
-def sep_num(num):
-    # regex that matches `num` not surrounded by any other numbers
-    # (so coins(2) won't match e.g. 123)
-    return fr"(?<!\d){num}(?!\d)"
+check50.include("StudentsPerformance.csv")
+assert os.path.exists("StudentsPerformance.csv")
 
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
@@ -16,9 +14,6 @@ def exists(): # the name of the check
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases11(exists):
     """Program prints correct output"""
-    
-    check50.include("StudentsPerformance.csv")
-    assert os.path.exists("StudentsPerformance.csv")
     
     actual = check50.run("python3 Question1.py").stdout()
     expected = "The group of students with the highest mean scores in math, writing, and reading had a majority of female students."
