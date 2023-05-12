43c21ed85e6674a7cb4ee7b780ec0af003349526
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-12 09:54:16 -0400

    Update foo.py

--- 4_Pandas/Question2/foo.py
+++ 4_Pandas/Question2/foo.py
@@ -1,6 +1,7 @@
 import check50 # import the check50 module
 import check50.py
 import re
+import os
 
 def sep_num(num):
     # regex that matches `num` not surrounded by any other numbers
@@ -12,3 +13,16 @@ def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
     check50.exists("Question2.py") # the actual check
         
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases1(exists):
+    """Program prints correct output 1"""
+    
+    check50.include("StudentsPerformance.csv")
+    assert os.path.exists("StudentsPerformance.csv")
+    
+    actual = check50.run("python3 Question1.py").stdout()
+    expected = "7    98\n8    45\n9    98"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for first math scores print out."
+        raise check50.Missing("Correct results",'your output',help=help)
