971f91c239e0260cdd34f037f1da5425e142a01b
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-12 17:28:53 -0400

    Update foo.py

--- 4_Pandas/Question2/foo.py
+++ 4_Pandas/Question2/foo.py
@@ -22,3 +22,15 @@ def check_cases1(exists):
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for first math scores print out."
         raise check50.Missing("Correct results",'your output',help=help)
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases2(exists):
+    """Program prints correct output 2"""
+    
+    actual = check50.run("python3 Question2.py").stdout()
+    expected = "1004    97\n1005    96\n1006    56"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for the second math scores print out."
+        raise check50.Missing("Correct results",'your output',help=help)
