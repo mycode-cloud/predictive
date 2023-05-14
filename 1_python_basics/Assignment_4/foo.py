71f749301b4259ad31978cdca4ed28c9adfc93ea
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 11:11:04 -0400

    Update foo.py

--- 1_python_basics/Assignment_4/foo.py
+++ 1_python_basics/Assignment_4/foo.py
@@ -10,15 +10,15 @@ def sep_num(num):
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question4.py") # the actual check
+    check50.exists("Assignment_4.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases():
     """Program prints correct number of points for organizer - Test 1"""
-    check50.include("Question4_Sol.py")
-    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    check50.include("Assignment_4_Sol.py")
+    check50.py.append_code("Assignment_4.py", "Assignment_4_Sol.py")
     
-    actual = check50.run("python3 Question4.py").stdout()
+    actual = check50.run("python3 Assignment_4.py").stdout()
     expected1 = "25\n"
     
     if not re.search(sep_num(25), actual):
@@ -33,10 +33,10 @@ def check_cases():
 @check50.check(check_cases) # only run this check if the exists check has passed
 def check_cases2():
     """Program prints correct number of points for organizer - Test 2"""
-    check50.include("Question4_Sol.py")
-    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    check50.include("Assignment_4_Sol.py")
+    check50.py.append_code("Assignment_4.py", "Assignment_4_Sol.py")
     
-    actual = check50.run("python3 Question4.py").stdout()
+    actual = check50.run("python3 Assignment_4.py").stdout()
     expected2 = "5\n"
     
     if not re.search(sep_num(5), actual):
@@ -50,10 +50,10 @@ def check_cases2():
 @check50.check(check_cases2) # only run this check if the exists check has passed
 def check_cases3():
     """Program prints correct number of points for organizer - Test 3"""
-    check50.include("Question4_Sol.py")
-    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    check50.include("Assignment_4_Sol.py")
+    check50.py.append_code("Assignment_4.py", "Assignment_4_Sol.py")
     
-    actual = check50.run("python3 Question4.py").stdout()
+    actual = check50.run("python3 Assignment_4.py").stdout()
     expected3 = "55\n"
     
     if not re.search(sep_num(55), actual):
@@ -68,10 +68,10 @@ def check_cases3():
 @check50.check(check_cases3) # only run this check if the exists check has passed
 def check_cases4():
     """Program prints correct number of points for organizer - Test 4"""
-    check50.include("Question4_Sol.py")
-    check50.py.append_code("Question4.py", "Question4_Sol.py")
+    check50.include("Assignment_4_Sol.py")
+    check50.py.append_code("Assignment_4.py", "Assignment_4_Sol.py")
     
-    actual = check50.run("python3 Question4.py").stdout()
+    actual = check50.run("python3 Assignment_4.py").stdout()
     expected4 = "35\n"
     
     if not re.search(sep_num(35), actual):
