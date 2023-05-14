2cfae2300736e0876cb9612a17e8e46695b001a3
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 11:31:14 -0400

    Update foo.py

--- 5_Visual/Assignment_7/foo.py
+++ 5_Visual/Assignment_7/foo.py
@@ -4,5 +4,5 @@ import check50.py
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question5.py") # the actual check
+    check50.exists("Assignment_7.py") # the actual check
     
