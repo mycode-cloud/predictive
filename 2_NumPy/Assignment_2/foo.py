a94e496127b0e54b626602e7c90917860c280867
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 11:15:39 -0400

    Update foo.py

--- 2_NumPy/Assignment_2/foo.py
+++ 2_NumPy/Assignment_2/foo.py
@@ -5,13 +5,13 @@ import re
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question2.py") # the actual check
+    check50.exists("Assignment_2.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases11(exists):
     """Program prints correct output for subarray of 'a'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[1 2\]\n \[5 6\]\]"
     
     if not re.search(expected, actual):
@@ -24,7 +24,7 @@ def check_cases11(exists):
 def check_cases12(exists):
     """Program prints correct output for subarray of 'b'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[27 28\]\n \[31 32\]\]"
     
     if not re.search(expected, actual):
@@ -37,7 +37,7 @@ def check_cases12(exists):
 def check_cases13(exists):
     """Program prints correct output for difference 'a-b'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[-26 -26\]\n \[-26 -26\]\]"
     
     if not re.search(expected, actual):
@@ -51,7 +51,7 @@ def check_cases13(exists):
 def check_cases14(exists):
     """Program prints correct output for elementwise multiplication of subarrays for 'a' and 'b'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[ 27  56\]\n \[155 192\]\]"
     
     if not re.search(expected, actual):
@@ -64,7 +64,7 @@ def check_cases14(exists):
 def check_cases15(exists):
     """Program prints correct output for elementwise division of subarrays for 'a' and 'b'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[0.03703704 0.07142857\]\n \[0.16129032 0.1875    \]\]"
     
     if not re.search(expected, actual):
@@ -77,7 +77,7 @@ def check_cases15(exists):
 def check_cases16(exists):
     """Program prints correct output for sum of elements in subarrays for 'a' and 'b'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "132"
     
     if not re.search(expected, actual):
@@ -104,7 +104,7 @@ def check_cases16(exists):
 def check_cases21(exists):
     """Program prints correct output for subarray of 'c'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[11 21\]\n \[51 61\]\]"
     
     if not re.search(expected, actual):
@@ -117,7 +117,7 @@ def check_cases21(exists):
 def check_cases22(exists):
     """Program prints correct output for subarray of 'd'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[227 228\]\n \[231 232\]\]"
     
     if not re.search(expected, actual):
@@ -130,7 +130,7 @@ def check_cases22(exists):
 def check_cases23(exists):
     """Program prints correct output for difference 'c-d'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[-216 -207\]\n \[-180 -171\]\]"
     
     if not re.search(expected, actual):
@@ -144,7 +144,7 @@ def check_cases23(exists):
 def check_cases24(exists):
     """Program prints correct output for elementwise multiplication of subarrays for 'c' and 'd'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[ 2497  4788\]\n \[11781 14152\]\]"
     
     if not re.search(expected, actual):
@@ -157,7 +157,7 @@ def check_cases24(exists):
 def check_cases25(exists):
     """Program prints correct output for elementwise division of subarrays for 'c' and 'd'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[0.04845815 0.09210526\]\n \[0.22077922 0.26293103\]\]"
     
     if not re.search(expected, actual):
@@ -170,7 +170,7 @@ def check_cases25(exists):
 def check_cases26(exists):
     """Program prints correct output for sum of elements in subarrays for 'c' and 'd'"""
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "1062"
     
     if not re.search(expected, actual):
@@ -193,24 +193,24 @@ def check_cases26(exists):
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases3(exists):
     """Program prints correct outputs for other arrays"""
-    check50.include("Question2_Sol.py")
-    check50.py.append_code("Question2.py", "Question2_Sol.py")
+    check50.include("Assignment_2_Sol.py")
+    check50.py.append_code("Assignment_2.py", "Assignment_2_Sol.py")
     
-    actual = check50.run("python3 Question2.py").stdout()
+    actual = check50.run("python3 Assignment_2.py").stdout()
     expected = "\[\[113 213\]\n \[513 613\]\]"
     
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for general first subarray."
         raise check50.Missing("General first subarray",'your output',help=help)
         
-    actual00 = check50.run("python3 Question2.py").stdout()
+    actual00 = check50.run("python3 Assignment_2.py").stdout()
     expected00 = "\[\[2272 2282\]\n \[2312 2322\]\]"
     
     if not re.search(expected00, actual00):
         help = r"Your code does not print the correct result for general second subarray."
         raise check50.Missing("General second subarray",'your output',help=help)
 
-    actual0 = check50.run("python3 Question2.py").stdout()
+    actual0 = check50.run("python3 Assignment_2.py").stdout()
     expected0 = "\[\[-2159 -2069\]\n \[-1799 -1709\]\]"
     
     if not re.search(expected0, actual0):
@@ -218,21 +218,21 @@ def check_cases3(exists):
         raise check50.Missing("General difference of subarrays",'your output',help=help)
         
         
-    actual2 = check50.run("python3 Question2.py").stdout()
+    actual2 = check50.run("python3 Assignment_2.py").stdout()
     expected2 = "\[\[ 256736  486066\]\n \[1186056 1423386\]\]"
     
     if not re.search(expected2, actual2):
         help = r"Your code does not print the correct result for elementwise multiplication for general subarrays."
         raise check50.Missing("General multiplication subarray",'your output',help=help)
         
-    actual3 = check50.run("python3 Question2.py").stdout()
+    actual3 = check50.run("python3 Assignment_2.py").stdout()
     expected3 = "\[\[0.04973592 0.09333918\]\n \[0.22188581 0.26399655\]\]"
     
     if not re.search(expected3, actual3):
         help = r"Your code does not print the correct result for the elementwise division of general subarrays."
         raise check50.Missing("General division subarray",'your output',help=help)
         
-    actual4 = check50.run("python3 Question2.py").stdout()
+    actual4 = check50.run("python3 Assignment_2.py").stdout()
     expected4 = "10640"
     
     if not re.search(expected4, actual4):
