b8292c1dbfcf00f2c25822729db9a16976778933
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 11:28:02 -0400

    Update foo.py

--- 4_Pandas/Assignment_5/foo.py
+++ 4_Pandas/Assignment_5/foo.py
@@ -10,6 +10,6 @@ def sep_num(num):
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question3.py") # the actual check
+    check50.exists("Assignment_5.py") # the actual check
 
         
