cd19322193ecf7523b1b672dfc8f8ff049812d6a
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 10:48:09 -0400

    Update foo.py

--- 1_python_basics/Assignment_1/foo.py
+++ 1_python_basics/Assignment_1/foo.py
@@ -5,12 +5,12 @@ import check50.py
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question1.py") # the actual check
+    check50.exists("Assignment_1.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases():
     """Program prints correct name length - Test 1"""
-    actual = check50.run("python3 Question1.py").stdin("XXXXX").stdin("XXXXX").stdout()
+    actual = check50.run("python3 Assignment_1.py").stdin("XXXXX").stdin("XXXXX").stdout()
     expected = "10"
     
     if not re.search(expected, actual):
@@ -20,7 +20,7 @@ def check_cases():
 @check50.check(check_cases) # only run this check if the exists check has passed
 def check_cases2():
     """Program prints correct name length - Test 2"""
-    actual = check50.run("python3 Question1.py").stdin("XXXXX").stdin("XXXXXXXXXX").stdout()
+    actual = check50.run("python3 Assignment_1.py").stdin("XXXXX").stdin("XXXXXXXXXX").stdout()
     expected = "15"
     
     if not re.search(expected, actual):
@@ -30,7 +30,7 @@ def check_cases2():
 @check50.check(check_cases2) # only run this check if the exists check has passed
 def check_cases3():
     """Program prints correct name length - Test 3"""
-    actual = check50.run("python3 Question1.py").stdin("XXXXXXXXXX").stdin("XXXXXXXXXXX").stdout()
+    actual = check50.run("python3 Assignment_1.py").stdin("XXXXXXXXXX").stdin("XXXXXXXXXXX").stdout()
     expected = "21"
     
     if not re.search(expected, actual):
