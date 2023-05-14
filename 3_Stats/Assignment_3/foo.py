e1b92edf9100aa61b901dc4e24d4a3c4842a2a42
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 11:19:43 -0400

    Update foo.py

--- 3_Stats/Assignment_3/foo.py
+++ 3_Stats/Assignment_3/foo.py
@@ -10,13 +10,13 @@ def sep_num(num):
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question3.py") # the actual check
+    check50.exists("Assignment_3.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases():
     """Program prints the correct variance and standard deviation for 'speeds' - Test 1"""
     
-    actual = check50.run("python3 Question3.py").stdout()
+    actual = check50.run("python3 Assignment_n3.py").stdout()
     expected1 = "[26.05885057  5.10478703]"
     
     if not re.search(expected1, actual):
@@ -31,7 +31,7 @@ def check_cases():
 def check_cases2():
     """Program prints the correct variance and standard deviation for 'speeds2' - Test 2"""
     
-    actual = check50.run("python3 Question3.py").stdout()
+    actual = check50.run("python3 Assignment_3.py").stdout()
     expected2 = "[30.506      5.5232237]"
     
     if not re.search(expected2, actual):
@@ -45,10 +45,10 @@ def check_cases2():
 @check50.check(check_cases2) # only run this check if the exists check has passed
 def check_cases3():
     """Program prints the correct variance and standard deviation for random data speeds - Test 3"""
-    check50.include("Question3_Sol.py")
-    check50.py.append_code("Question3.py", "Question3_Sol.py")
+    check50.include("Assignment_3_Sol.py")
+    check50.py.append_code("Assignment_3.py", "Assignment_3_Sol.py")
     
-    actual = check50.run("python3 Question3.py").stdout()
+    actual = check50.run("python3 Assignment_3.py").stdout()
     expected3 = "[34.85742857  5.904018  ]"
     
     if not re.search(expected3, actual):
