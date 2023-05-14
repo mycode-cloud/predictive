404373e8e0179b28be6f25535bb1832922948dca
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 10:45:41 -0400

    File updates

---
+++ 4_Pandas/Assignment_5/foo.py
@@ -0,0 +1,15 @@
+import check50 # import the check50 module
+import check50.py
+import re
+
+def sep_num(num):
+    # regex that matches `num` not surrounded by any other numbers
+    # (so coins(2) won't match e.g. 123)
+    return fr"(?<!\d){num}(?!\d)"
+
+@check50.check() # tag the function below as check50 check
+def exists(): # the name of the check
+    """File exists""" # this is what you will see when running check50
+    check50.exists("Question3.py") # the actual check
+
+        
