810d1ed2a35bedc365e73e98177536320b9f8e5c
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-14 10:49:17 -0400

    Update foo.py

--- 1_python_basics/Assignment_3/foo.py
+++ 1_python_basics/Assignment_3/foo.py
@@ -5,15 +5,15 @@ import re
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Question3.py") # the actual check
+    check50.exists("Assignment_3.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases():
     """Program prints correct list - Test 1"""
-    check50.include("Question3_Sol.py")
-    check50.py.append_code("Question3.py", "Question3_Sol.py")
+    check50.include("Assignment_3_Sol.py")
+    check50.py.append_code("Assignment_3.py", "Assignment_3_Sol.py")
     
-    actual = check50.run("python3 Question3.py").stdout()
+    actual = check50.run("python3 Assignment_3.py").stdout()
     
     if "Johannes Jamal Galina" not in actual:
         help = r"Your code does not work with the list ['Johannes', 'Jamal', 'Jamal', 'Johannes', 'Galina']."
@@ -24,10 +24,10 @@ def check_cases():
 @check50.check(check_cases) # only run this check if the exists check has passed
 def check_cases2():
     """Program prints correct list - Test 2"""
-    check50.include("Question3_Sol.py")
-    check50.py.append_code("Question3.py", "Question3_Sol.py")
+    check50.include("Assignment_3_Sol.py")
+    check50.py.append_code("Assignment_3.py", "Assignment_3_Sol.py")
     
-    actual = check50.run("python3 Question3.py").stdout()
+    actual = check50.run("python3 Assignment_3.py").stdout()
     
     if "a b c" not in actual:
         help = r"Your code does not work with the list ['a', 'a', 'a', 'b', 'b', 'c'].  Your code is being checked against several different lists."
@@ -40,10 +40,10 @@ def check_cases2():
 @check50.check(check_cases2) # only run this check if the exists check has passed
 def check_cases3():
     """Program prints correct list - Test 3"""
-    check50.include("Question3_Sol.py")
-    check50.py.append_code("Question3.py", "Question3_Sol.py")
+    check50.include("Assignment_3_Sol.py")
+    check50.py.append_code("Assignment_3.py", "Assignment_3_Sol.py")
     
-    actual = check50.run("python3 Question3.py").stdout()
+    actual = check50.run("python3 Assignment_3.py").stdout()
     
     if "d e f g" not in actual:
         help = r"Your code does not work with a random list.  Your code is being checked against several different lists."
