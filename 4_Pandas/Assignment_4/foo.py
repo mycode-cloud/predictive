73fc1eea895cb09239e6fbf6f8669f29f93fc0b5
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-15 18:11:53 -0400

    Update foo.py

--- 4_Pandas/Assignment_4/foo.py
+++ 4_Pandas/Assignment_4/foo.py
@@ -32,7 +32,7 @@ def check_cases2(exists):
     assert os.path.exists("Canada_YouTube_Videos.csv")
     
     actual = check50.run("python3 Assignment_4.py").stdout()
-    expected = "Index(['video_id', 'trending_date', 'title', 'channel_title', 'category_id', 'publish_time'"
+    expected = "Index(['video_id', 'trending_date', 'title', 'channel_title', 'category_id', 'publish_time',\n"
     
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for the updated list of columns after using the drop method."
