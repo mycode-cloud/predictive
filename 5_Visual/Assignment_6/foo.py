ab03a910f0b93afa6eac121015b1d5a2abcf6c4c
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-25 15:26:59 -0400

    Update foo.py

--- 5_Visual/Assignment_6/foo.py
+++ 5_Visual/Assignment_6/foo.py
@@ -16,7 +16,7 @@ def check_cases1(exists):
     assert os.path.exists("Big_Mart_Numerical.csv")
     
     actual = check50.run("python3 Assignment_6.py").stdout()
-    expected = "7          NaN         0\.127470          4022\.7636"
+    expected = r"\bNaN\b.*\b0\.127470\b.*\b4022\.7636\b"
     
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for the first print out of 10 rows of 'bmn' using the '.head()' method."
