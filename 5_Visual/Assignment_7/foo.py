421e67541f82ec1109a6beac78f67950c09eb946
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-26 17:47:15 -0400

    Update foo.py

--- 5_Visual/Assignment_7/foo.py
+++ 5_Visual/Assignment_7/foo.py
@@ -16,7 +16,7 @@ def check_cases1():
     assert os.path.exists("Big_Mart.csv")
     actual = check50.run("python3 Assignment_7.py").stdout(timeout=20)
     
-    expected = "Item_Fat_Content\nLow Fat    2774\nRegular    1575\nLF          177\nreg          71\nlow fat      53"
+    expected = r"Item_Fat_Content\nLow Fat\s+(?:\b2774\b|\b53\b)\nRegular\s+(?:\b1575\b|\b\d\d\b)\nLF\s+\b177\b\nreg\s+\b71\b\nlow fat\s+(?:\b2774\b|\b53\b)"
     
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for the first print out of .value_counts for the Item_Fat_Content column."
