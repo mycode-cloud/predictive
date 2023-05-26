c2266ddae8e21ada35810a45973f5a6240454cc5
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-26 13:13:24 -0400

    Update foo.py

--- 5_Visual/Assignment_6/foo.py
+++ 5_Visual/Assignment_6/foo.py
@@ -3,16 +3,14 @@ import check50.py
 import re
 import os
 
+check50.include("Big_Mart_Numerical.csv")
+assert os.path.exists("Big_Mart_Numerical.csv")
 actual = check50.run("python3 Assignment_6.py").stdout(timeout=10)
 
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
     check50.exists("Assignment_6.py") # the actual check
-    
-    check50.include("Big_Mart_Numerical.csv")
-    assert os.path.exists("Big_Mart_Numerical.csv")
-
         
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases1():
