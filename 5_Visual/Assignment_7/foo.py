ac5ba5021a2c443c161a91ba84eebfe1758e89b8
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-26 18:19:02 -0400

    Update foo.py

--- 5_Visual/Assignment_7/foo.py
+++ 5_Visual/Assignment_7/foo.py
@@ -36,3 +36,17 @@ def check_cases2():
         help = r"Your code does not print the correct result for the second print out of .value_counts for the Item_Fat_Content column."
         raise check50.Missing("Correct results",'your output',help=help)
         
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases3():
+    """Program prints correct output 3"""
+    
+    check50.include("Big_Mart.csv")
+    assert os.path.exists("Big_Mart.csv")
+    actual = check50.run("python3 Assignment_7.py").stdout(timeout=20)
+    
+    expected = r"\'Item_Fat_Content_Low_Fat\', \'Item_Fat_Content_Regular\'"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for the first print out of the column names in bm_one using the tolist() method."
+        raise check50.Missing("Correct results",'your output',help=help)
