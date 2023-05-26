4fb2419ddb5c7861deb7f912a7b417c275bfb785
mycode-cloud <brendan.mclellan@gmail.com>
2023-05-26 18:13:20 -0400

    Update foo.py

--- 5_Visual/Assignment_7/foo.py
+++ 5_Visual/Assignment_7/foo.py
@@ -22,3 +22,17 @@ def check_cases1():
         help = r"Your code does not print the correct result for the first print out of .value_counts for the Item_Fat_Content column."
         raise check50.Missing("Correct results",'your output',help=help)
         
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases2():
+    """Program prints correct output 2"""
+    
+    check50.include("Big_Mart.csv")
+    assert os.path.exists("Big_Mart.csv")
+    actual = check50.run("python3 Assignment_7.py").stdout(timeout=20)
+    
+    expected = "Low Fat\s*3004\s*\nRegular\s*1646"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for the second print out of .value_counts for the Item_Fat_Content column."
+        raise check50.Missing("Correct results",'your output',help=help)
+        
