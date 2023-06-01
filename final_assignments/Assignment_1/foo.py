a5e71286e04e787170a96fe263ad116c7a42fccb
mycode-cloud <brendan.mclellan@gmail.com>
2023-06-01 16:36:29 -0400

    Update foo.py

--- final_assignments/Assignment_1/foo.py
+++ final_assignments/Assignment_1/foo.py
@@ -13,33 +13,9 @@ def check_cases1():
     
     actual = check50.run("python3 Assignment_1.py").stdout(timeout=20)
     
-    expected = r"\s*\[ 0\.  0\.  0\. \.\.\. 16\.  9\.  0\.\]"
+    expected = r"1005202\s*\$1\,060\s*\$212\s*45\.0\s*49\.0\s*0\.40"
     
     if not re.search(expected, actual):
-        help = r"Your code does not print the correct result for the first print out 'x'."
+        help = r"Your code does not print the correct result for the first print out of abnb.head(10)."
         raise check50.Missing("Correct results",'your output',help=help)
         
-@check50.check(exists) # only run this check if the exists check has passed
-def check_cases2():
-    """Program prints correct output 2"""
-   
-    actual = check50.run("python3 Assignment_1.py").stdout(timeout=20)
-    
-    expected = "0\.9992044550517104"
-    
-    if not re.search(expected, actual):
-        help = r"Your code does not print the correct result for model.score(x_train,y_train)."
-        raise check50.Missing("Correct results",'your output',help=help)
-        
-        
-@check50.check(exists) # only run this check if the exists check has passed
-def check_cases3():
-    """Program prints correct output 3"""
-   
-    actual = check50.run("python3 Assignment_1.py").stdout(timeout=20)
-    
-    expected = r"\s*\[ 0  0  0  0  0  0  0  0  2 57\]\]"
-    
-    if not re.search(expected, actual):
-        help = r"Your code does not print the correct result for the first confusion matrix print out."
-        raise check50.Missing("Correct results",'your output',help=help)
