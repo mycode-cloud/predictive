266de50a0d92ee69f1a288fe42c9bee418c9fb2e
mycode-cloud <brendan.mclellan@gmail.com>
2023-06-01 16:40:44 -0400

    Update foo.py

--- final_assignments/Assignment_1/foo.py
+++ final_assignments/Assignment_1/foo.py
@@ -23,3 +23,19 @@ def check_cases1():
         help = r"Your code does not print the correct result for the first print out of abnb.head(10)."
         raise check50.Missing("Correct results",'your output',help=help)
         
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases2():
+    """Program prints correct output 2"""
+    
+    check50.include("Airbnb_1.csv")
+    assert os.path.exists("Airbnb_1.csv")
+   
+    actual = check50.run("python3 Assignment_1.py").stdout(timeout=20)
+    
+    expected = "The shape of the original DataFrame is: \(9999, 7\)"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for the shape of the original DataFrame."
+        raise check50.Missing("Correct results",'your output',help=help)
+        
