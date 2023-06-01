2e5977a390f9499a458921c9b0b2636bd0b19c51
mycode-cloud <brendan.mclellan@gmail.com>
2023-06-01 16:44:48 -0400

    Update foo.py

--- final_assignments/Assignment_1/foo.py
+++ final_assignments/Assignment_1/foo.py
@@ -55,3 +55,20 @@ def check_cases3():
         help = r"Your code does not print the correct result for the shape of the updated DataFrame."
         raise check50.Missing("Correct results",'your output',help=help)
         
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases4():
+    """Program prints correct output 4"""
+    
+    check50.include("Airbnb_1.csv")
+    assert os.path.exists("Airbnb_1.csv")
+   
+    actual = check50.run("python3 Assignment_1.py").stdout(timeout=20)
+    
+    expected = "All column headings of the DataFrame with spaces replaced by underscores are: Index\(\[\'id\', \'price\', \'service_fee\'"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for the column headings of the DataFrame with spaces replaced by underscores."
+        raise check50.Missing("Correct results",'your output',help=help)
+        
