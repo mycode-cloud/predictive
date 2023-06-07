478eac48a36d83d8a372ce93ff0f7892ea98b8ec
mycode-cloud <brendan.mclellan@gmail.com>
2023-06-07 16:42:26 -0400

    Update foo.py

--- final_assignments/Assignment_2/foo.py
+++ final_assignments/Assignment_2/foo.py
@@ -31,3 +31,16 @@ def check_cases2():
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for the shape of the DataFrame after dropping all rows containing empty entries."
         raise check50.Missing("Correct results",'your output',help=help)
+        
+        
+@check50.check(exists) # only run this check if the exists check has passed
+def check_cases3():
+    """Program prints correct output 3"""
+   
+    actual = check50.run("python3 Assignment_2.py").stdout(timeout=20)
+    
+    expected = "The R-squared valued for price vs\. minimum_nights is\s*0\.0007275938878409383"
+    
+    if not re.search(expected, actual):
+        help = r"Your code does not print the correct result for the print out the R-squared value for price vs. minimum_nights."
+        raise check50.Missing("Correct results",'your output',help=help)
