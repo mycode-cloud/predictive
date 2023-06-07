c7801682e59a89a5400fbee3c4e0ac64a6e38614
mycode-cloud <brendan.mclellan@gmail.com>
2023-06-07 16:44:22 -0400

    Update foo.py

--- final_assignments/Assignment_2/foo.py
+++ final_assignments/Assignment_2/foo.py
@@ -39,7 +39,7 @@ def check_cases3():
    
     actual = check50.run("python3 Assignment_2.py").stdout(timeout=20)
     
-    expected = "The R-squared valued for price vs\. minimum_nights is\s*0\.0007275938878409383"
+    expected = "The R-squared valued for price vs\.\s*minimum_nights\s*is\s*0\.0007275938878409383"
     
     if not re.search(expected, actual):
         help = r"Your code does not print the correct result for the print out the R-squared value for price vs. minimum_nights."
