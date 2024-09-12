12a54d5d6395745a0510b9e5074c6119b7e97b3d
mycode-cloud <brendan.mclellan@gmail.com>
2024-09-12 16:20:55 -0400

    Update foo.py

--- 1_python_basics/Assignment_2/foo.py
+++ 1_python_basics/Assignment_2/foo.py
@@ -5,7 +5,7 @@ import re
 @check50.check() # tag the function below as check50 check
 def exists(): # the name of the check
     """File exists""" # this is what you will see when running check50
-    check50.exists("Assignment2.py") # the actual check
+    check50.exists("Assignment_2.py") # the actual check
 
 @check50.check(exists) # only run this check if the exists check has passed
 def check_cases():
